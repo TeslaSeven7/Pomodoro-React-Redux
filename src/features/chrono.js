21bd75240981b67f23ae7b66fa98ee1841ec08fd
TeslaSeven7 <alex.aubrier@epitech.eu>
2023-10-01 22:53:12 +0200

    Added Redux MiddleWare and logic for toggle button

--- src/features/chrono.js
+++ src/features/chrono.js
@@ -1,7 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	id: 1,
 	session: {
 		value: 1500,
 		runningValue: 1500,
@@ -21,6 +20,48 @@ const initialState = {
 export const chrono = createSlice({
 	name: 'chrono',
 	initialState,
-	reducers: {},
+	reducers: {
+		updateChronoValues: (state, action) => {
+			const chosenState = state[action.payload.type];
+			//block below 1
+			if (chosenState.value + action.payload.value === 0) return;
+			if (action.payload.type === 'session') {
+				if (!state.isPLaying) {
+					chosenState.value = chosenState.value + action.payload.value;
+					chosenState.runningValue =
+						chosenState.runningValue + action.payload.value;
+					state.displayedValue.value = chosenState.runningValue;
+				} else {
+					chosenState.value = chosenState.value + action.payload.value;
+				}
+			} else if (action.payload.type === 'pause') {
+				chosenState.value = chosenState.value + action.payload.value;
+			}
+		},
+		tick: (state, action) => {
+			return;
+		},
+		setUpChrono: (state, action) => {
+			state.isPLaying = true;
+			state.intervalID = action.payload;
+			console.log(state.isPLaying);
+		},
+		resetChrono: (state, action) => {
+			window.clearInterval(state.intervalID);
+			state.isPLaying = false;
+			console.log(state.isPLaying);
+		},
+	},
 });
+export function startChrono(action) {
+	return function (dispatch, getState) {
+		const intervalID = setInterval(() => {
+			dispatch(tick());
+		}, 1000);
+		dispatch(setUpChrono(intervalID));
+		dispatch(tick());
+	};
+}
+
+export const { updateChronoValues, setUpChrono, resetChrono } = chrono.actions;
 export default chrono.reducer;
